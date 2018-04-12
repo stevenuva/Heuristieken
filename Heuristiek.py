5541a5e21061b0445f72445117426b395a64df10
Steven020 <s.r.kuhnenff@gmail.com>
2018-04-13 01:23:27 +0200

    update git link

--- Heuristiek.py
+++ Heuristiek.py
@@ -1,36 +1,35 @@
-
-# coding: utf-8
-
-# In[3]:
-
-
 import csv
+import os
 
-# change to github link
-csv_file = "C:/Users/steve/Desktop/Heuristieken/CargoList1.csv"
+# github link to retrieve CargoList1.csv if necessary
+github_link = ("https://github.com/stevenuva/minor-programmeren/blob/"
+               "master/CargoList1.csv")
+
+# path to the csv_file
+csv_file = "./data/CargoList1.csv"
+
+for path in [csv_file]:
+    assert os.path.exists(path), (f"{path} does not exist. Please download file"
+                                  f" from {github_link}")
 
 # load csv file into a file
-with open(csv_file, 'r') as infile:
+with open(csv_file, "r") as infile:
     reader = csv.reader(infile)
     cargo_list1 = list(reader)
 
-# create an empty dictionary 
+# create an empty dictionary
 cargo1_dic = {}
 
 # loop through list to store csv data into a dictionary
-for row in cargo_list1:    
+for row in cargo_list1:
     key = row[0]
     if key in cargo1_dic:
         pass
     cargo1_dic[key] = row[1:]
-    
+
 # csv file turned dictionary
 # values: weight(kg), volume(m^3)
 print(cargo1_dic)
-    
+
 # gets the values of a parcel with the id "CL1#1"
 print(cargo1_dic.get("CL1#1"))
-
-
-
-
