67e0f6b6d938ddddca97a05fb7e26bfe4a8fd253
Michael Hu <maikel-f1fan@live.nl>
2018-04-24 14:00:56 +0200

    heuristiek.py weer normaal

--- Heuristiek.py
+++ Heuristiek.py
@@ -3,10 +3,10 @@ import os
 
 # github link to retrieve CargoList1.csv if necessary
 github_link = ("https://github.com/stevenuva/minor-programmeren/blob/"
-               "master/CargoList2.csv")
+               "master/CargoList1.csv")
 
 # path to the csv_file
-csv_file = "./data/CargoList2.csv"
+csv_file = "./data/CargoList1.csv"
 
 # check if user has csv at the right path
 for path in [csv_file]:
@@ -34,7 +34,7 @@ cargo1_list = sorted(cargo1_list, key=lambda
                      parcel: parcel["volume"])
 
 # slice list
-cargo1_list = cargo1_list[:70]
+cargo1_list = cargo1_list[:84]
 
 # sort list on kg/m3 ratio
 cargo1_list = sorted(cargo1_list, key=lambda
@@ -65,61 +65,24 @@ counter2 = 0
 counter3 = 0
 counter4 = 0
 
-# create spacecraft cargo
-c_spacecraft1 = {}
-c_spacecraft2 = {}
-c_spacecraft3 = {}
-c_spacecraft4 = {}
-
-key = 0
-
-# for parcel in cargo1_list:
-#     if(parcel["mass"] <= list_spacecrafts[2]["mass"]) and (parcel["volume"] <= list_spacecrafts[2]["volume"]):
-#         list_spacecrafts[2]["mass"] -= parcel["mass"]
-#         list_spacecrafts[2]["volume"] -= parcel["volume"]
-#         counter3 += 1
-
-#     elif(parcel["mass"] <= list_spacecrafts[3]["mass"]) and (parcel["volume"] <= list_spacecrafts[3]["volume"]):
-#         list_spacecrafts[3]["mass"] -= parcel["mass"] 
-#         list_spacecrafts[3]["volume"] -= parcel["volume"]
-#         counter4 += 1
-#     elif(parcel["mass"] <= list_spacecrafts[1]["mass"]) and (parcel["volume"] <= list_spacecrafts[1]["volume"]):
-#         list_spacecrafts[1]["mass"] -= parcel["mass"]
-#         list_spacecrafts[1]["volume"] -= parcel["volume"]
-#         counter2 += 1
-#     elif(parcel["mass"] <= list_spacecrafts[0]["mass"]) and (parcel["volume"] <= list_spacecrafts[0]["volume"]):
-#         list_spacecrafts[0]["mass"] -= parcel["mass"]
-#         list_spacecrafts[0]["volume"] -= parcel["volume"]
-#         counter1 += 1
-
-
-
 # loop to place the parcels in the spaceship when possible
 for parcel in cargo1_list:
     if(parcel["mass"] <= list_spacecrafts[0]["mass"]) and (parcel["volume"] <= list_spacecrafts[0]["volume"]):
         list_spacecrafts[0]["mass"] -= parcel["mass"]
         list_spacecrafts[0]["volume"] -= parcel["volume"]
-        key += 1 
-        c_spacecraft1[key] = parcel
         counter1 += 1
 
     elif(parcel["mass"] <= list_spacecrafts[1]["mass"]) and (parcel["volume"] <= list_spacecrafts[1]["volume"]):
         list_spacecrafts[1]["mass"] -= parcel["mass"] 
         list_spacecrafts[1]["volume"] -= parcel["volume"]
-        key += 1 
-        c_spacecraft2[key] = parcel
         counter2 += 1
     elif(parcel["mass"] <= list_spacecrafts[2]["mass"]) and (parcel["volume"] <= list_spacecrafts[2]["volume"]):
         list_spacecrafts[2]["mass"] -= parcel["mass"]
         list_spacecrafts[2]["volume"] -= parcel["volume"]
-        key += 1 
-        c_spacecraft3[key] = parcel
         counter3 += 1
     elif(parcel["mass"] <= list_spacecrafts[3]["mass"]) and (parcel["volume"] <= list_spacecrafts[3]["volume"]):
         list_spacecrafts[3]["mass"] -= parcel["mass"]
         list_spacecrafts[3]["volume"] -= parcel["volume"]
-        key += 1 
-        c_spacecraft4[key] = parcel
         counter4 += 1
     else:
         print("remaining: ", parcel)
@@ -133,10 +96,5 @@ print("Parcels in the fourth spaceship:", counter4, "->Remaining:", list_spacecr
 counter = counter1 + counter2 + counter3 + counter4
 
 print("Total parcels :", counter)
-
-print("\nSpacecraft1 Cargo: ", c_spacecraft1)
-print("\nSpacecraft2 Cargo: ",c_spacecraft2)
-print("\nSpacecraft3 Cargo: ", c_spacecraft3)
-print("\nSpacecraft4 Cargo: ", c_spacecraft4)
 
 
