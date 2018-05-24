6f21b074bc75e2368eb2777eda3ab59c6c169c70
Michael Hu <maikel-f1fan@live.nl>
2018-05-24 16:22:38 +0200

    randomgreedy

--- algorithms/test.py
+++ algorithms/test.py
@@ -1,1 +1,29 @@
-import matplotlib.pyplot as plt
+# json.dump(Cygnus.remaining_mass, output)
+# json.dump("-", output)
+# json.dump(Cygnus.remaining_volume, output)
+# json.dump("-", output)
+# json.dump(Progress.remaining_mass, output)
+# json.dump("-", output)
+# json.dump(Progress.remaining_volume, output)
+# json.dump("-", output)
+# json.dump(Kounotori.remaining_mass, output)
+# json.dump("-", output)
+# json.dump(Kounotori.remaining_volume, output)
+# json.dump("-", output)
+# json.dump(Dragon.remaining_mass, output)
+# json.dump("-", output)
+# json.dump(Dragon.remaining_volume, output)
+#
+# json.dump("Cygnus_cost: " + str(Cygnus.cost()), output)
+# json.dump(" - ", output)
+#
+# json.dump("Progress_cost: "+ str(Progress.cost()), output)
+# json.dump(" - ", output)
+#
+# json.dump("Kounotori_cost: "+ str(Kounotori.cost()), output)
+# json.dump(" - ", output)
+#
+# json.dump("Dragon_cost: "+ str(Dragon.cost()), output)
+# json.dump(" - ", output)
+
+# json.dump("Total cost: ", str(total_cost), output)
